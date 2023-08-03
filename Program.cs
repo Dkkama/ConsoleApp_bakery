1784e7d49e2a17aa033c1547ecca1cb3e607ef61
Kamilya <103946198+Dkkama@users.noreply.github.com>
2023-08-03 16:47:36 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,10 +11,33 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        static void cupCakeFunction(string pathName) // we declare the datatype only while declaring e.g. a function
+        {
+            Console.WriteLine("Calories: ");
+            string cupCal = Console.ReadLine();
+
+            Console.WriteLine("Flavour: ");
+            string cupFlav = Console.ReadLine();
+
+            Console.WriteLine("Ingrediants: ");
+            string cupIngr = Console.ReadLine();
+            Cupcake cup = new Cupcake(cupCal, cupFlav, cupIngr);
+
+            File.AppendAllText(pathName, string.Join("; ", ("Cupcake", cup.Calory, cup.Flavour, cup.Ingrediants))); 
+        }
+        static void cakeFunction(string pathName)
+        {
+            Console.WriteLine("Calories: ");
+            string cakeCal = Console.ReadLine(); // int cakeCal = int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Flavour: ");
+            string cakeFlav = Console.ReadLine();
+            Cake cake = new Cake(cakeCal, cakeFlav); // obj cake is created
+
+            File.AppendAllText(pathName, string.Join("; ", ("Cake", cake.Calory, cake.Flavour)));
+        }
         static void Main(string[] args)
         {
-            // FileStream fileStr = File.Create(pathName); // Create()
-
             string pathName = "C:\\Users\\Sulpak\\source\\repos\\ConsoleApp1\\ConsoleAppProject\\myFile.txt";
 
             Console.WriteLine("Name: ");
@@ -24,38 +47,19 @@ namespace ConsoleApp1
             string custLast = Console.ReadLine();
             Customer customer = new Customer(custName, custLast);
 
+            File.WriteAllText(pathName, string.Join("; ", (customer.FirstName, customer.LastName))); // TO DO: designate 
+
             Console.WriteLine("What do u prefer? Cake - 1; Cupcake - 2 ");
             int sweet = int.Parse(Console.ReadLine());
-            if (sweet == 1)
+            switch (sweet)
             {
-                Console.WriteLine("Calories: ");
-                string cakeCal = Console.ReadLine(); // int cakeCal = int.Parse(Console.ReadLine()); 
-
-                Console.WriteLine("Flavour: ");
-                string cakeFlav = Console.ReadLine();
-                Cake cake = new Cake(cakeCal, cakeFlav); // obj cake is created
-
-                File.WriteAllText(pathName, string.Join("; ", (customer.FirstName, customer.LastName, cake.Calory, cake.Flavour)));
-            }
-            else
-            {
-                Console.WriteLine("Calories: ");
-                string cupCal = Console.ReadLine();
-
-                Console.WriteLine("Flavour: ");
-                string cupFlav = Console.ReadLine();
-
-                Console.WriteLine("Ingrediants: ");
-                string cupIngr = Console.ReadLine();
-                Cupcake cup = new Cupcake(cupCal, cupFlav, cupIngr);
-
-                File.WriteAllText(pathName, string.Join("; ", (customer.FirstName, customer.LastName, cup.Calory, cup.Flavour, cup.Ingrediants)));
+                case 1: cakeFunction(pathName); 
+                    break;
+                default: cupCakeFunction(pathName);
+                    break;
             }
 
-
-            // Console.WriteLine(string.Join("; ", (customer.FirstName, customer.LastName, cake.Calory, cake.Flavour, cup.Calory, cup.Flavour, cup.Ingrediants)));
-            // File.WriteAllText(pathName, string.Join("; ", (customer.FirstName, customer.LastName, cake.Calory, cake.Flavour, cup.Calory, cup.Flavour, cup.Ingrediants))); // WriteAllText() 
-
+            
             Console.ReadKey();
         }
     }
