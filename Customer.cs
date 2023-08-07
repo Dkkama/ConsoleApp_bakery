284e58d80314930b6d5cb4b42d52ea1fa5ff830a
Kamilya <103946198+Dkkama@users.noreply.github.com>
2023-08-07 11:11:18 +0600

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -13,16 +13,20 @@ namespace ConsoleApp1
         /*private string firstname; // field
         public string Firstname; // property- combination of a variable and a method */
 
-        public Customer(string firstname, string lastname)
+        public Customer(string firstname, string lastname, int numCake, int numCup)
         {
             FirstName = firstname;
             LastName = lastname;
-        } // void - doesnt returns
+ 
+            NumCake = numCake;
+            NumCup = numCup;
+        } // void - doesnt return
 
         public string FirstName { get; set; } // FirstName - property 
         public string LastName { get; set; }
 
-        
+        public int NumCake { get; set; }
+        public int NumCup { get; set;}
 
     }
 }
